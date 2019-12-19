8954c1d891679b3f36407f43a4de003dcda0422a
Marius Phoenix <phoenixzenomarius@gmail.com>
2019-12-19 04:26:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,6 @@
 def introduction (name)
   puts  "Hi, my name is #{name}."
+<<<<<<< HEAD
 end
 
 def introduction_with_language (name, language)
@@ -8,6 +9,13 @@ end
 
 def introduction_with_language_optional (name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+end
+
+def introduction_with_language (name, language)
+  puts "salut"
+  
+>>>>>>> 999b3cdcfa315c84b1a281ef9ccfa5abc83b7359
 end
 
 
